caff72c0d5f85b64523b66ea12adaf1db803302b
jahnavi <jahnavirao19@gmail.com>
2020-03-03 20:41:53 +0530

    All the changes

--- main/templates/thealertbar.py
+++ main/templates/thealertbar.py
@@ -1,21 +1,30 @@
-﻿<!DOCTYPE html>
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 21:28:00 2020
+
+@author: buzz buzz
+"""
+
+!DOCTYPE html>
 <html>
 	<body>
 {% extends "dropdown.html" %}
 {% block content %}
 		<div class="login">
             <h1>USER LOGIN</h1>
-            {% if error %}
-                <p><strong>Error:</strong> {{ error }}
-            {% endif %}
             <form action="{{ url_for('userlogin') }}" method="POST">
     			<input type="text" name="username" placeholder="Username" id="username" required>
 				<input type="password" name="password" placeholder="Password" id="password" required>
 				<div class="msg">{{ msg }}</div>
-                <button>Login</button>				
-              
- 
+                <button onclick="redirect()">Login</button>				
 			</form>
+            <script>
+            var redirect = function() {
+                 setTimeout(function(){ alert("Login successfull"); 
+                     window.location.href = "http://localhost:5000/logout/";
+                     }, 5);
+                 };
+        </script>
  
             
 	     </div>
